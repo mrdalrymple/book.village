0bf60f95e699741255f66c28c9d66966cd075685
Matthew Dalrymple <matt.dalrymple@gmail.com>
2024-03-18 01:21:34 -0700

    kinda working

--- .build/osb.py
+++ .build/osb.py
@@ -2,9 +2,8 @@ from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
 
-# Open the text file
-with open('book.txt', 'r', encoding="utf-8") as file:
-    text = file.read()
+
+#def way_one(file)
 
 # Create a PDF document
 doc = SimpleDocTemplate('output.pdf', pagesize=letter)
@@ -12,7 +11,27 @@ doc = SimpleDocTemplate('output.pdf', pagesize=letter)
 # Create a list of flowable objects (Paragraphs)
 styles = getSampleStyleSheet()
 flowables = []
-flowables.append(Paragraph(text, styles['BodyText']))
+
+# Open the text file
+last_line = None
+with open('book.txt', 'r', encoding="utf-8") as file:
+    #text = file.read()
+    text = []
+    current_paragraph = None
+    for line in file.readlines():
+        if last_line is None:
+            text = []
+        elif last_line == "\n":
+            flowables.append(Paragraph("\n".join(text), styles['BodyText']))
+            text = []
+
+        text.append(line)
+        last_line = line
+
+if text:
+    flowables.append(Paragraph("\n".join(text), styles['BodyText']))
+
+#flowables.append(Paragraph(text, styles['BodyText']))
 
 # Build the PDF
 doc.multiBuild(flowables)
