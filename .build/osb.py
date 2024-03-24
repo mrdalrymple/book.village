4b7e2bb9843219a95ccdfe29d7c619ef80696d4b
Matthew Dalrymple <matt.dalrymple@gmail.com>
2024-03-23 20:44:23 -0700

    working

--- .build/osb.py
+++ .build/osb.py
@@ -1,6 +1,9 @@
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.enums import TA_JUSTIFY
+#from reportlab.lib.units import inch
 
 
 #def way_one(file)
@@ -18,18 +21,25 @@ with open('book.txt', 'r', encoding="utf-8") as file:
     #text = file.read()
     text = []
     current_paragraph = None
+    # for line in file.readlines():
+    #     if last_line is None:
+    #         text = []
+    #     elif last_line == "\n":
+    #         flowables.append(Paragraph("\n".join(text), styles['BodyText']))
+    #         text = []
+
+    #     text.append(line)
+    #     last_line = line
+
+    #paragraph_style = styles['BodyText']
+    paragraph_style = ParagraphStyle('BodyText', alignment=TA_JUSTIFY, leading=24)
     for line in file.readlines():
-        if last_line is None:
-            text = []
-        elif last_line == "\n":
-            flowables.append(Paragraph("\n".join(text), styles['BodyText']))
-            text = []
+        flowable = Paragraph(line, paragraph_style)
+        #flowable = DoubleSpacedObject(flowable)
+        flowables.append(flowable)
 
-        text.append(line)
-        last_line = line
-
-if text:
-    flowables.append(Paragraph("\n".join(text), styles['BodyText']))
+#if text:
+#    flowables.append(Paragraph("\n".join(text), styles['BodyText']))
 
 #flowables.append(Paragraph(text, styles['BodyText']))
 
